dca5b012d91168e59a6cd80b673bcbd1001cead0
vanessa conti <vanessaconti@pursuit.org>
2021-08-13 12:35:25 -0400

    id function easiest so far commit

--- index.js
+++ index.js
@@ -116,7 +116,14 @@ const exampleMovies = require("./movies");
       // Toy Story 4
     };
  */
-function findById() {}
+    function findById(movies, id) {
+      for(let movie of movies){
+        if(movie.imdbID === id) {
+          return movie; 
+        } 
+      }
+      return null;
+    }
 
 /**
  * filterByGenre()
